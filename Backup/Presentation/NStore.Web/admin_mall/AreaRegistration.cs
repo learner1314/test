8990d248a6986ef26256297999ba161ab471725f
吴诗锦 <wsj42207@ly.com>
2018-11-02 15:26:20 +0800

    Signed-off-by: 吴诗锦 <wsj42207@ly.com>

---
+++ Backup/Presentation/NStore.Web/admin_mall/AreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NStore.Web.MallAdmin
+{
+    public class AreaRegistration : System.Web.Mvc.AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "MallAdmin";
+            }
+        }
+
+        public override void RegisterArea(System.Web.Mvc.AreaRegistrationContext context)
+        {
+            //此路由不能删除
+            context.MapRoute("MallAdmin_default",
+                              "MallAdmin/{controller}/{action}",
+                              new { controller = "home", action = "index", area = "MallAdmin" },
+                              new[] { "NStore.Web.MallAdmin.Controllers" });
+
+        }
+    }
+}
